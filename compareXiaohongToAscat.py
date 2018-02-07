a6fc38182b892861bc527ea4517c1b471e040b79
Lucian Smith <lpsmith@uw.edu>
2018-02-06 17:39:35 -0800

    Update to clean up script, and to report percentages.

--- compareXiaohongToAscat.py
+++ compareXiaohongToAscat.py
@@ -29,128 +29,12 @@ if BAF_links:
 else:
     BAF_dir = "pASCAT_input_combined_all/"
 
-subdirs = ["diploid", "tetraploid"]
-#subdirs = ["diploid"]
-subdirdict = {}
-for subdir in subdirs:
-    subdirdict[subdir] = []
 gamma_outdir = "gamma_test_output/"
-#gamma_outdir = "gamma_test_output/"
 outdir = "gamma_test_output/analysis_compare/"
-#gamma_list = ["100", "150", "200", "250", "300", "350", "400", "450", "500", "600"]
-#gamma_list = ["50"]
-#gamma_list = ["Test"]
-#gamma_list = ["100", "500", "1000", "3000"]
 gamma_list = ["0", "50", "100", "150", "200", "250", "300", "350", "400", "450", "500", "600", "700", "800", "900", "1000", "1200", "1400", "1600", "2000", "2500", "3000"]
-#gamma_list = []
-bafrawdata = {}
-patient_samples = {}
 
 onlyonepatient = False
 onepatient = "163"
-
-#twopatients = [("521", "252")]
-#samples from patient 360:
-onepatientsamples = ["360_21620", "360_18370", "360_18354", "360_18358", "360_21608", "360_21606", "360_21604", "360_21602", "360_18356", "360_21600", "360_24412", "360_18350", "360_18444", "360_18446", "360_18352", "360_18366", "360_21618", "360_21598", "360_18362", "360_21596", "360_21614", "360_21594", "360_21616", "360_21592", "360_21610", "360_21612", "360_24409", "360_24810", "360_24813", "360_18372"]
-ignore_cnvis = True
-mirror_percentages = False
-
-#median for patient 521:  .5154885
-#Reasonable cutoffs for 521: 0.48 and 0.563 (about 750k left each)
-#A more agressive but balanced cutoff:  0.45 and 0.6  (about 215k left each)
-#The best cutoff is no cutoff at all:  0.5 and 0.5
-bafErrorBarLow = 0.5
-bafErrorBarHigh = 0.5
-
-print("Using BAF filtering for comparison from ", str(bafErrorBarLow), "to", str(bafErrorBarHigh))
-
-bafWtLow = 0.4
-bafWtHigh = 0.65
-
-#original cutoff: 0.35 to 0.65, or 0.15 out from 0.5.  The above values account for dye bias.
-
-def readBafNormal(patient):
-    bafrawdata = {}
-    bafwt = {}
-#    allcnvis = []
-    if BAF_links:
-        bafnormal = readlink(BAF_dir + patient + "_Normal_BAF.txt")
-    else:
-        bafnormal = BAF_dir + patient + "_Normal_BAF.txt"
-    if not(isfile(bafnormal)):
-        print("ERROR:  no Normal BAF file found for patient", patient)
-        return ({}, {})
-    bafnormal = open(bafnormal, "r")
-    print("Reading BAF normal data for patient", patient)
-    for line in bafnormal:
-        lvec = line.split()
-        if line.find("Chr") != -1:
-            continue
-        if ignore_cnvis and line.find("cnvi") != -1:
-            continue
-        try:
-            value = float(lvec[3])
-        except:
-            continue
-#        if (line.find("cnvi") != -1):
-#            allcnvis.append(value)
-        if (value < bafWtLow or value > bafWtHigh):
-            continue
-        chr = lvec[1].split('"')[1]
-        pos = int(lvec[2])
-        if chr not in bafrawdata:
-            bafrawdata[chr] = {}
-        if chr not in bafwt:
-            bafwt[chr] = {}
-        bafrawdata[chr][pos] = {}
-        bafwt[chr][pos] = value
-    bafnormal.close()
-#    print("All normal CNVI bafs:")
-#    lsl.createPrintAndSaveHistogram(allcnvis, "", .01)
-    return bafrawdata, bafwt
-
-def readBafSamples(baffile, bafrawdata):
-    labels = []
-    all_samples = []
-    if BAF_links:
-        baffile = readlink(BAF_dir + patient + "_BAF.txt")
-    else:
-        baffile = BAF_dir + patient + "_BAF.txt"
-    if not(isfile(baffile)):
-        print("ERROR:  no BAF file found for patient", patient)
-        bafrawdata = {}
-        return
-    print("Reading BAF sample data for patient", patient)
-    baffile = open(baffile, "r")
-    allbafs = {}
-    allbafs["cnvi"] = []
-    allbafs["normal"] = []
-    for line in baffile:
-        lvec = line.split()
-        if line.find("Chr") != -1:
-            labels = lvec
-            for l in range(2,len(labels)):
-                all_samples.append(labels[l].split('"')[1])
-            continue
-        chr = lvec[1].split('"')[1]
-        pos = int(lvec[2])
-        if chr not in bafrawdata:
-            continue
-        if pos not in bafrawdata[chr]:
-            continue
-        for p in range(3,len(lvec)):
-            sample = labels[p-1].split('"')[1]
-            try:
-                bafrawdata[chr][pos][sample] = float(lvec[p])
-                if line.find("cnvi") != -1:
-                    allbafs["cnvi"].append(float(lvec[p]))
-                else:
-                    bafrawdata[chr][pos][sample] = float(lvec[p])
-                    allbafs["normal"].append(float(lvec[p]))
-            except:
-                continue
-    baffile.close()
-    return allbafs, all_samples
 
 def getIsegsFromCopynumberFileFor(patient):
     #This function reads the lowest-gamma-value copynumber file it can, reads it, and returns it as 'isegs'.
@@ -308,100 +192,6 @@ def readAllXiaohongSegmentation():
             readXiaohongCopynumFile(Xdir_1M + f, Xiaohong_segments)
     return Xiaohong_segments
 
-def writeBalanceFiles(isegs, patient, all_samples):
-    for sample in all_samples:
-        outfile = open(outdir + sample + "_balanced_calls.txt", "w")
-        outfile.write("Patient\tSample\tChr\tStart\tEnd\tCall\n")
-        for chr in isegs:
-            for iseg in isegs[chr]:
-                outfile.write(patient)
-                outfile.write("\t" + sample.split("_")[1])
-                outfile.write("\t" + chr)
-                outfile.write("\t" + str(iseg[0]))
-                outfile.write("\t" + str(iseg[1]))
-                matches = iseg[2]
-                nonmatches = iseg[3]
-                sample_in_matches = False
-                sample_in_nonmatches = False
-                for match in matches:
-                    if sample in match:
-                        sample_in_matches = True
-                        break
-                for nonmatch in nonmatches:
-                    if sample in nonmatch:
-                        sample_in_nonmatches = True
-                        break
-                if sample_in_matches:
-                    outfile.write("\tUnbalanced")
-                elif not sample_in_nonmatches:
-                    outfile.write("\tShort")
-                elif len(matches)>0:
-                    outfile.write("\tBalanced")
-                else:
-                    outfile.write("\tUnknown")
-                outfile.write("\n")
-
-def writeSummary(isegs, patient, all_samples, all_analyses):
-    if not onlyonepatient and isfile(outdir + patient + "_analysis_overview.txt"):
-        print("Skipping patient", patient, ": analysis already exists.")
-        return
-    overview = {}
-    overview_bases = {}
-    calls = ["TP", "FP", "UP", "TN", "FN", "UN", "NC", "S"]
-    for sample in all_samples:
-        overview[sample] = {}
-        overview_bases[sample] = {}
-        for analysis in all_analyses:
-            overview[sample][analysis] = {}
-            overview_bases[sample][analysis] = {}
-            for call in calls:
-                overview[sample][analysis][call] = 0
-                overview_bases[sample][analysis][call] = 0
-    summary_out = open(outdir + patient + "_full_analysis.txt", "w")
-    summary_out.write("Patient")
-    summary_out.write("\tSample")
-    summary_out.write("\tchr")
-    summary_out.write("\tstart")
-    summary_out.write("\tend")
-    for analysis in all_analyses:
-        summary_out.write("\t" + analysis)
-    summary_out.write("\n")
-    for sample in all_samples:
-        for chr in isegs:
-            for iseg in isegs[chr]:
-                summary_out.write(patient)
-                summary_out.write("\t" + sample.split("_")[1])
-                summary_out.write("\t" + chr)
-                summary_out.write("\t" + str(iseg[0]))
-                summary_out.write("\t" + str(iseg[1]))
-                for analysis in all_analyses:
-                    summary_out.write("\t" + iseg[4][sample][analysis])
-                    overview[sample][analysis][iseg[4][sample][analysis]] += 1
-                    overview_bases[sample][analysis][iseg[4][sample][analysis]] += iseg[1] - iseg[0]
-                summary_out.write("\n")
-    summary_out.close()
-
-    overview_out = open(outdir + patient + "_analysis_overview.txt", "w")
-    overview_out.write("Patient")
-    overview_out.write("\tSample")
-    overview_out.write("\tAnalysis")
-    for call in calls:
-        overview_out.write("\t" + call + "_num")
-    for call in calls:
-        overview_out.write("\t" + call + "_len")
-    overview_out.write("\n")
-    for sample in all_samples:
-        for analysis in all_analyses:
-            overview_out.write(patient)
-            overview_out.write("\t" + sample.split("_")[1])
-            overview_out.write("\t" + analysis)
-            for call in calls:
-                overview_out.write("\t" + str(overview[sample][analysis][call]))
-            for call in calls:
-                overview_out.write("\t" + str(overview_bases[sample][analysis][call]))
-            overview_out.write("\n")
-    overview_out.close()
-
 def getCanonicalAscatCallsFor(patient):
     if patient=="163":
         return [("24735", "900", "tetraploid"), ("23740", "900", "tetraploid"), ("23743", "800", "tetraploid"), ("23749", "200", "tetraploid")]
@@ -452,15 +242,32 @@ def readAscatSegmentationFor(patient, canon):
 
 def getCallFor(start, end, seg):
     (tstart, tend, call) = seg
-    if start >= tstart and end <= tend:
-        return call
-    if start < tstart and end > tstart:
-        if end - tstart > tstart - start:
-            return "Mostly_" + call
-    if start < tend and end > tend:
-        if end - tend < tend - start:
-            return "Mostly_" + call
-    return ""
+    overlap = max(0, (min(end, tend) - max(start, tstart))) / (end-start)
+    return (call, overlap)
+
+def getSummary(calls, wtNotCalled):
+    if len(calls)==1:
+        return calls[0][0]
+    if len(calls)==0:
+        if wtNotCalled:
+            return "wt?"
+        return "??"
+    totcalls = 0
+    summary = {}
+    for call in calls:
+        totcalls += call[1]
+        callname = call[0]
+        if callname not in summary:
+            summary[callname] = 0
+        summary[callname] += call[1]
+    if totcalls < 0.8 and wtNotCalled:
+        summary["wt"] = 1-totcalls
+    if len(summary.keys()) == 1:
+        return list(summary.keys())[0]
+    ret = ""
+    for sumtype in summary:
+        ret += sumtype + "_" + str(round(summary[sumtype], 2)) + "_"
+    return ret
 
 def writeComparison(Xsegs, Asegs, patient, sample, gamma, ploidy, isegs):
     compareout = open(outdir + "compare_xiaohong_to_ascat_" + patient + "_" + sample + "_g" + gamma + "_" + ploidy + ".txt", "w")
@@ -477,19 +284,19 @@ def writeComparison(Xsegs, Asegs, patient, sample, gamma, ploidy, isegs):
     for chr in isegs:
         sumdiff = 0
         for (start, end) in isegs[chr]:
-            xcall = ""
-            acall = ""
+            xcalls = []
+            acalls = []
             if chr in Xsegs[patient][sample]:
                 for xseg in Xsegs[patient][sample][chr]:
                     xcall = getCallFor(start, end, xseg)
-                    if xcall != "":
-                        break
+                    if xcall[1] > 0:
+                        xcalls.append(xcall)
             for aseg in Asegs[chr]:
                 acall = getCallFor(start, end, aseg)
-                if acall != "":
-                    break
-            if xcall == "":
-                xcall = "wt?"
+                if acall[1] > 0:
+                    acalls.append(acall)
+            xcall = getSummary(xcalls, True)
+            acall = getSummary(acalls, False)
             
             compareout.write(patient)
             compareout.write("\t" + sample)
