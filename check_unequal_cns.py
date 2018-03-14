b883ab6dc24a7b24a7f4f299c11945bab3b9b37c
Lucian Smith <lpsmith@uw.edu>
2018-03-14 08:34:41 -0700

    commits

--- check_unequal_cns.py
+++ check_unequal_cns.py
@@ -14,60 +14,77 @@ from os import mkdir
 import lucianSNPLibrary as lsl
 
 CNdir = "nonintegerCNs/"
+balanced_dir = "balanced_calls/"
 
 onlysomepatients = False
 somepatients = ["997"]
 
 firstpatients = ["17", "42", "55", "59", "74", "43", "184", "163", "396", "1047"]
 
-files = []
-firstmore = 0
-secondmore = 0
-firstmore_fromeq = 0
-secondmore_fromeq = 0
-firstmore_zeroB = 0
-zeroB = 0
-zeroB_zeroA = 0
-total = 0
-for (__, __, f) in walk(CNdir):
-    files += f
-for f in files:
-    if "nonint" not in f:
-        continue
-    (patient, sample, ploidy) = f.split("_")[0:3]
-    #print(patient, sample, ploidy, ":")
+
+def readBalancedCalls(patient, sample):
+    balfile = open(balanced_dir + patient + "_" + sample + "_balanced_calls.tsv", "r")
+    balcalls = {}
+    for line in balfile:
+        if "Chr" in line:
+            continue
+        (fpatient, fsample, chr, start, end, call) = line.rstrip().split()
+        if chr not in balcalls:
+            balcalls[chr] = {}
+        balcalls[chr][(int(start), int(end))] = call
+    return balcalls
+
+
+def readAmbiguousCallsFromASCAT(f):
     cnfile = open(CNdir + f, "r")
+    unbal_calls = {}
     for line in cnfile:
         if "patient" in line:
             continue
         (patient, sample, chr, start, end, rawA, rawB, intA, intB) = line.split()
         if intA == "NA" or intB == "NA":
             continue
-        total += 1
-        intA = int(intA)
-        intB = int(intB)
-        if rawB == "0":
-            zeroB += 1
-            if rawA == "0" or intA == 0:
-                zeroB_zeroA += 1
-        if rawA == rawB and intA != intB:
-            if intA > intB:
-                firstmore_fromeq += 1
-            else:
-                secondmore_fromeq += 1
-                print(line)
-        elif intA != intB:
-            if intA > intB:
-                firstmore += 1
-                if rawB == "0":
-                    firstmore_zeroB += 1
-                else:
-                    print(line)
-            else:
-                secondmore += 1
+        rawA = float(rawA)
+        rawB = float(rawB)
+        if abs(rawA-rawB) < 0.000001 and intA != intB:
+            if chr not in unbal_calls:
+                unbal_calls[chr] = []
+            unbal_calls[chr].append((int(start), int(end), rawA))
+    return unbal_calls
 
-print("First more:", firstmore)
-print("Second more:", secondmore)
-print("First more from equal raw values:", firstmore_fromeq)
-print("Second more from equal raw values:", secondmore_fromeq)
-print("Total segments:", total)
+def compareAndReport(unbal_calls, bal_calls, patient, sample, ploidy, allbal, allunbal):
+    for chr in unbal_calls:
+        for ub_seg in unbal_calls[chr]:
+            for b_seg in bal_calls[chr]:
+                if bal_calls[chr][b_seg] == "Unknown":
+                    continue
+                (bstart, bend) = b_seg
+                (ubstart, ubend, rawA) = ub_seg
+                if bstart >= ubstart and bend <= ubend:
+                    if bal_calls[chr][b_seg] == "Balanced":
+                        allbal.append(rawA)
+#                        print("Balanced:", chr, ub_seg)
+                    else:
+                        allunbal.append(rawA)
+#                        print("Unbalanced:", chr, ub_seg)
+
+allbal = []
+allunbal = []
+files = []
+for (__, __, f) in walk(CNdir):
+    files += f
+for f in files:
+    if "nonint" not in f:
+        continue
+    (patient, sample, ploidy) = f.split("_")[0:3]
+    if onlysomepatients and patient not in somepatients:
+        continue
+    unbal_calls = readAmbiguousCallsFromASCAT(f)
+    bal_calls = readBalancedCalls(patient, sample)
+    print ("Comparing", patient, sample, ploidy)
+    compareAndReport(unbal_calls, bal_calls, patient, sample, ploidy, allbal, allunbal)
+
+print("All balanced values:")
+lsl.createPrintAndSaveHistogram(allbal, "", 0.001)
+print("All unbalanced values:")
+lsl.createPrintAndSaveHistogram(allunbal, "", 0.001)
