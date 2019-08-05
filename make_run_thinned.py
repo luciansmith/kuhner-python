96d8e563252053b4f4a80540a2d9644eb9328b55
Lucian Smith <lpsmith@uw.edu>
2019-08-05 12:13:31 -0700

    Update to create all phylogeny trees, and scale a few more of them, too.

--- make_run_thinned.py
+++ make_run_thinned.py
@@ -6,7 +6,9 @@ Created on Mon Jul  1 15:29:33 2019
 @author: lpsmith
 """
 
-ntips = 50
+rootdir = "tsinfer_1xr/"
+reactionRate = "2.679985"
+ntips = 1000
 x2tips = ntips*2
 
 runall = open("run_all.bat", "w")
@@ -17,12 +19,12 @@ for i in ["0"]:
             simout = open("sim_thinned_" + ijk + ".sge", "w")
             runall.write("qsub -l mfree=4G sim_thinned_" + ijk + ".sge\n")
             simout.write("module load modules modules-init modules-gs gcc/latest python/latest numpy/1.16.0 scipy/1.2.0 glibc/2.14\n")
-            simout.write("cd tsinfer_2xr/" + str(ntips) + "tip_thinned/\n")
+            simout.write("cd " + rootdir + str(ntips) + "tip_thinned/\n")
             simout.write("rm reccount\n")
             simout.write("rm migcount\n")
             simout.write("rm totreccount\n")
             simout.write("rm RFresults\n")
-            simout.write("# ./ms " + str(x2tips) + " 1 -T -t 550.000000 -r 19.800000 100000 | tail -n +4 | grep -v // > truetree" + ijk + "\n")
+            simout.write("# ./ms " + str(x2tips) + " 1 -T -t 550.000000 -r " + reactionRate + " 100000 | tail -n +4 | grep -v // > truetree" + ijk + "\n")
             simout.write("python3 truncate_ms_tree.py truetree" + ijk + " treefile" + ijk + "\n")
             simout.write("python thinned.py treefile" + ijk + " thinnedtree" + ijk + " " + str(ntips) + "\n")
             simout.write("./seq-gen -mF84 -l 100000 -s 0.005500 -p 1000 thinnedtree" + ijk + " > dnafile" + ijk + "\n")
@@ -38,15 +40,31 @@ runall.close()
 
 initout = open("sim_init_trees.sge", "w")
 initout.write("module load modules modules-init modules-gs gcc/latest python/latest numpy/1.16.0 scipy/1.2.0 glibc/2.14\n")
-initout.write("cd tsinfer_2xr/" + str(ntips) + "tip_thinned/\n")
+initout.write("cd " + rootdir + str(ntips) + "tip_thinned/\n")
 initout.write("for i in 0\n")
 initout.write("do\n")
 initout.write("for j in 0 1 2 3 4 5 6 7 8 9\n")
 initout.write("do\n")
 initout.write("for k in 0 1 2 3 4 5 6 7 8 9\n")
 initout.write("do\n")
-initout.write("  ./ms " + str(x2tips) + " 1 -T -t 550.000000 -r 19.800000 100000 | tail -n +4 | grep -v // > truetree$i$j$k\n")
+initout.write("  ./ms " + str(x2tips) + " 1 -T -t 550.000000 -r " + reactionRate + " 100000 | tail -n +4 | grep -v // > truetree$i$j$k\n")
 initout.write("done\n")
 initout.write("done\n")
 initout.write("done\n")
 initout.close()
+
+argout = open("sim_argcompare.sge", "w")
+argout.write("module load modules modules-init modules-gs gcc/latest python/latest numpy/1.16.0 scipy/1.2.0 glibc/2.14\n")
+argout.write("cd " + rootdir + str(ntips) + "tip_thinned/\n")
+argout.write("rm RFresults\n")
+argout.write("for i in 0\n")
+argout.write("do\n")
+argout.write("for j in 0 1 2 3 4 5 6 7 8 9\n")
+argout.write("do\n")
+argout.write("for k in 0 1 2 3 4 5 6 7 8 9\n")
+argout.write("do\n")
+argout.write("  python ts_argcompare.py thinnedtree$i$j$k inferredtree$i$j$k\n")
+argout.write("done\n")
+argout.write("done\n")
+argout.write("done\n")
+argout.close()
--- show_phylogeny_trees.py
+++ show_phylogeny_trees.py
@@ -9,7 +9,11 @@ Created on Wed Jul  3 15:29:58 2019
 import os
 import ete3
 
-outdir = "non17_phylogenies/"
+#outdir = "non17_phylogenies/"
+#target_pids = [88, 130, 385, 541, 572, 729, 956, 42, 169, 279, 623, 728, 852, 951, 995]
+
+outdir = "all_phylogenies/"
+target_pids = [55, 59, 478, 635, 865, 126, 909, 381, 609, 184]
 
 # Ingest the trees.  First, grab all the filenames/ paths of the tree files
 tree_files = {'path': [], 'PatientID': []}
@@ -23,7 +27,6 @@ for x, pid in enumerate(tree_files['PatientID']):
         pid = 891
     tree_files['PatientID'][x] = int(pid)
     
-target_pids = [88, 130, 385, 541, 572, 729, 956, 42, 169, 279, 623, 728, 852, 951, 995]
 vertical_margins = {
         88:  25,
         130: 20, 
@@ -39,12 +42,31 @@ vertical_margins = {
         728: 20, 
         852: 25, 
         951: 25, 
-        995: 25}
+        995: 25,
+
+        55: 24, 
+        59: 18, 
+        126: 19, 
+        184: 12, 
+        381: 16, 
+        478: 12, 
+        609: 18, 
+        635: 20, 
+        865: 15, 
+        909: 15, 
+
+        286: 18, 
+        387: 26, 
+        521: 24, 
+        672: 33, 
+}
 
 for x, path in enumerate(tree_files['path']):
     pid = tree_files['PatientID'][x]
-    if pid not in target_pids:
-        continue
+    if pid not in vertical_margins:
+        vertical_margins[pid] = 20
+#    if pid not in target_pids:
+#        continue
 
     # And load the tree
     t = ete3.Tree(path)
@@ -61,7 +83,7 @@ for x, path in enumerate(tree_files['path']):
     tstyle.mode = 'r'
     tstyle.branch_vertical_margin = vertical_margins[pid]
     tstyle.show_scale = False
-    # Show the leaf names, and the branch lenghts
+    # Don't show the leaf names or print the branch lengths
     tstyle.show_leaf_name = False
     tstyle.show_branch_length = False
     for branch in t:
@@ -69,6 +91,7 @@ for x, path in enumerate(tree_files['path']):
             branch.delete()
     for branch in t:
         if "_" in branch.name:
+            #Print the leaf names in our own font
             name_face = ete3.AttrFace("name", fsize=vertical_margins[pid]+10)
             branch.add_face(name_face, column=0, position="branch-right")
 #        if "blood" in branch.name:
@@ -83,6 +106,7 @@ for x, path in enumerate(tree_files['path']):
     #display(Markdown('## Patient ' + str(pid)))
 
     #Somehow, '%%inline' isn't working any more; revert to just printing the basic tree with zero information (sad)
+    print("\nTree for ", pid, ":")
     print(t)
     #t.show()
     #print("['%%inline' not working for some reason; try later to get prettier trees]")
